a5623e8c707297722666ce4f7156c8d355cd8f08
EduhDev <eduardocarvalho970@gmail.com>
2019-11-12 22:39:12 -0200

    mobile spots list screen finalized

--- frontend/src/pages/New/index.js
+++ frontend/src/pages/New/index.js
@@ -1,16 +1,49 @@
-import React, { useState } from 'react';
+import React, { useState, useMemo } from 'react';
 
-export default function New() {
+import './styles.css';
+
+import api from '../../services/api';
+
+import cameraIcon from '../../assets/camera.svg';
+
+export default function New({history}) {
+  const [thumbnail, setThumbnail] = useState(null);
   const [company, setCompany] = useState('');
   const [techs, setTechs] = useState('');
   const [price, setPrice] = useState('');
 
-  const handleSubmit = () => {
+  const preview = useMemo(() => {
+    return thumbnail ? URL.createObjectURL(thumbnail) : null;
+  }, [thumbnail])
 
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    const data = new FormData();
+    const user_id = localStorage.getItem('user');
+
+    data.append('thumbnail', thumbnail);
+    data.append('company', company);
+    data.append('techs', techs);
+    data.append('price', price);
+
+    await api.post('/spots', data, {
+      headers: { user_id }
+    });
+    
+    history.push('/dashboard');
   }
 
   return (
     <form onSubmit={handleSubmit}>
+      <label id="thumbnail" 
+        style={{ backgroundImage: `url(${preview})`}}
+        className={thumbnail ? 'has-thumbnail' : ''}
+      >
+        <input type="file" onChange={e => setThumbnail(e.target.files[0   ] )}/>
+        <img src={cameraIcon} alt="Select img" />
+      </label>
+
       <label htmlFor="company">COMPANY *</label>
       <input 
         type="text" 
